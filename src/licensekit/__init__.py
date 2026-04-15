d52302716dc60a523b63c49961e05c0f574b463d
Ryan Julyan <ryan@julyan.biz>
2026-04-15 19:47:09 +0200

    bump version 0.1.3 -> 0.1.4

--- src/licensekit/__init__.py
+++ src/licensekit/__init__.py
@@ -29,7 +29,7 @@ from .io import (
 )
 from .context import LicenseContext
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 # Testing utilities - conditionally imported to avoid pytest dependency in production
 try:
